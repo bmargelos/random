b7af4310bdd7a7368b978066923e7f8024d5f7be
bmargelos <57237131+bmargelos@users.noreply.github.com>
2020-02-10 17:32:43 -0500

    hey

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+ class LinkedList1 {
+
+    public static void main(String[] args) {
+        //creating my a list of my favorite songs
+        //This Linked list is holding a list of strings
+
+        LinkedList<String> mList = new LinkedList<String>();
+
+        //adding data into music list
+
+        mList.add("11 pm");
+        mList.add("Sweeter");
+        mList.add("Con Altura");
+        mList.add("NASA");
+        mList.add("7 Rings");
+        mList.add("Gasolina");
+        mList.add("Circles");
+        mList.add("Distraction");
+        mList.add("Ring");
+        mList.add("HP");
+        mList.add("Crew");
+        mList.add("Questions");
+        mList.add("Bellyache");
+        mList.add("Hips don;t lie");
+        mList.add("X");
+
+        System.out.println(mList.get(0));
+
+
+    } //end of PSVM
+}//end of main class
